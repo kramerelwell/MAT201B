595d2d36e7238626089bd63d1a91f5f0f2e1068a
Kramer Elwell <kramerelwell@ucsb.edu>
2020-01-19 13:46:02 -0800

    200119 Assignment 1

---
+++ build/Ass1/Release/CMakeFiles/Ass1_run.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ass1_run"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Ass1_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
