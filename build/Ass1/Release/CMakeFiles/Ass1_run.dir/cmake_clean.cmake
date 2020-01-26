8bea9e62c2551b105f9b1747bd776ba95a47852f
Kramer Elwell <kramerelwell@ucsb.edu>
2020-01-25 22:37:56 -0800

    200125 House Cleaning

--- build/Ass1/Release/CMakeFiles/Ass1_run.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Ass1_run"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Ass1_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
